c058775e762f81f1e81fc2ce32a3e92a66e8c5b5
Willis <130226179+WinsperTech@users.noreply.github.com>
2024-05-14 10:38:09 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -62,14 +62,10 @@ _SESSION ID_
 
 - Above is your session ID. Copy it as it will be required during deploy.
 
-❒ Owner: Willis +2547862739455
-
+❒ Owner: Willis +254786273945
 ❒ Group: https://chat.whatsapp.com/KlFKWN3QWq04DKeAdjk8gw
-
 ❒ Channel: https://whatsapp.com/channel/0029VaZ8Q0Y1XquZ673Uvs0m
-
 ❒ Github: https://github.com/WinsperTech
-
 ❒ YouTube: www.youtube.com/@WillisKE
 
 Good Luck ✅
