d2b01999c85748257306c7c01ff732a90133fdcd
Willis <130226179+WinsperTech@users.noreply.github.com>
2024-05-25 09:13:57 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -33,7 +33,7 @@ router.get('/', async (req, res) => {
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: ["Firefox (Windows)", "", ""]
+                browser: ["Chrome (𓆩≛𓆪)", "", ""]
              });
              if(!Pair_Code_By_Maher_Zubair.authState.creds.registered) {
                 await delay(1500);
